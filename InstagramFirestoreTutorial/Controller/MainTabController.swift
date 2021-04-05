20eb5f1c4eaa244249b890ffa0395c5080ed42cd
gatherheart <gatherheart@naver.com>
2021-04-06 01:34:38 +0900

    [ADD] Configure Project

---
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by bean Milky on 2021/04/06.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
