4cbdc3e4c679847366c86984e1f8e90caa46357d
gatherheart <gatherheart@naver.com>
2021-04-07 01:46:55 +0900

    [ADD] Login Page UI

---
+++ InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by bean Milky on 2021/04/07.
+//
+
+import Foundation
---
+++ InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by bean Milky on 2021/04/07.
+//
+
+import Foundation
